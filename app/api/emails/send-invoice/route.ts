24dc28e8ac09f65254ef00414f7c9c86a6585195
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2024-07-03 20:33:12 +1000

    adding a check to see if the order was loaded

--- app/api/emails/send-invoice/route.ts
+++ app/api/emails/send-invoice/route.ts
@@ -1,7 +1,7 @@
-import Stripe from "stripe"
-import {headers} from "next/headers"
+import Stripe from "stripe";
+import { headers } from "next/headers";
 
-import stripe from "@/lib/stripe"
+import stripe from "@/lib/stripe";
 import { NextResponse } from "next/server";
 import { ResendClient, ResendResponse } from "@/lib/resend";
 import getOrder from "@/actions/getOrder";
@@ -12,36 +12,47 @@ export async function POST(req: Request) {
 
   let event: Stripe.Event;
 
-  const stripeSecret = process.env.STRIPE_WEBHOOK_SECRET!
+  const stripeSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
   try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      stripeSecret
-    )
+    event = stripe.webhooks.constructEvent(body, signature, stripeSecret);
   } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message} ::: Webhook Signature: ${signature} ::: Stripe Webhook Secret: ${stripeSecret}`, {status:400})
+    return new NextResponse(
+      `Webhook Error: ${error.message} ::: Webhook Signature: ${signature} ::: Stripe Webhook Secret: ${stripeSecret}`,
+      { status: 400 }
+    );
   }
 
-  const session = event.data.object as Stripe.Checkout.Session
+  const session = event.data.object as Stripe.Checkout.Session;
 
   if (event.type === "checkout.session.completed") {
-
     const orderId = session?.metadata?.orderId;
 
     if (orderId) {
       const order = await getOrder(orderId);
 
-      // Send email
-      const emailResult: ResendResponse = await ResendClient.sendCustomerInvoice(order.email, order);
-  
-      console.log("[CUSTOMER INVOICE]: ", emailResult.detail);
-  
-      return new NextResponse(JSON.stringify(emailResult), {status: 200})
-   
+      if (order) {
+        // Send email
+        const emailResult: ResendResponse =
+          await ResendClient.sendCustomerInvoice(
+            session.customer_email ?? "",
+            order
+          );
+
+        console.log("[CUSTOMER INVOICE]: ", emailResult.detail);
+
+        return new NextResponse(JSON.stringify(emailResult), {
+          status: emailResult.status,
+        });
+      }
+
+      return new NextResponse("Email not sent - Order not found", {
+        status: 500,
+      });
     }
   }
 
-  return new NextResponse("Email not sent - Something went wrong", {status: 400})
+  return new NextResponse("Email not sent - Something went wrong", {
+    status: 500,
+  });
 }
