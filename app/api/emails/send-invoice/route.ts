e1628172c671d972f78cb168f0ae12f6e82e5525
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2024-07-03 20:47:09 +1000

    Figured out order not sending bug, need to update the order with the webhook session details, otherwise the db wouldn't be updated yet from 021 webhook

--- app/api/emails/send-invoice/route.ts
+++ app/api/emails/send-invoice/route.ts
@@ -25,21 +25,44 @@ export async function POST(req: Request) {
 
   const session = event.data.object as Stripe.Checkout.Session;
 
+  const address = session.shipping_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
+
   if (event.type === "checkout.session.completed") {
     const orderId = session?.metadata?.orderId;
 
     if (orderId) {
-      const order = await getOrder(orderId);
+      let order = await getOrder(orderId);
 
       if (order) {
+        order = {
+          ...order,
+          paidOn: new Date(),
+          phone: session.customer_details?.phone || "",
+          address: addressString,
+          email: session.customer_details?.email || "",
+          name: session.customer_details?.name || "",
+          shipping: session.shipping_cost?.amount_total
+            ? session.shipping_cost?.amount_total / 100
+            : 0,
+          isLocalPickUp: session?.metadata?.localPickUp === "true",
+        };
+
         // Send email
         const emailResult: ResendResponse =
           await ResendClient.sendCustomerInvoice(order.email, order);
 
-        console.log(
-          "[CUSTOMER INVOICE]: ",
-          emailResult.detail + " - " + order.email
-        );
+        console.log("[CUSTOMER INVOICE]: ", emailResult.detail);
 
         return new NextResponse(JSON.stringify(emailResult), {
           status: emailResult.status,
